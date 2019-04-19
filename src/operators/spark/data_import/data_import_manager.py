e02506f19fffd59aae56a2a0658958ae9fae9ceb
Gu Hailin <linhaigu@gmail.com>
2019-04-19 17:10:15 +0800

    update

---
+++ src/operators/spark/data_import/data_import_manager.py
@@ -0,0 +1,31 @@
+from operators.operator_manager import OperatorManager
+
+from operators.spark.data_import.import_csv import ImportCSV
+
+class DataImportOperatorManager(OperatorManager):
+    '''A data import operator manager'''
+
+    MANAGER_NAME = 'data-import'
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.operators = {}
+
+    def register_manager(self, manager):
+        return None
+
+    def register_operator(self, operator_class):
+        self.operators[operator_class.OP_NAME] = operator_class
+
+    def get_manager(self, manager_name):
+        return None
+
+    def get_operator(self, op_name):
+        return self.operators[op_name]
+
+dataImportOperatorManager = DataImportOperatorManager()
+dataImportOperatorManager.register_operator(ImportCSV)
