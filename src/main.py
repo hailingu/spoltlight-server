b996c42e2daf2afed9e893d74b61c9c7aec4e033
hailingu <linhaigu@gmail.com>
2019-05-06 14:34:37 +0800

    update

--- src/main.py
+++ src/main.py
@@ -3,9 +3,11 @@ from flask_cors import CORS
 
 from flow.flow_manager import FlowManager
 from scheduler.scheduler_factory import SchedulerFactory
+from log.log_center import LogCenter
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
+
 
 @app.route('/submit_flow', methods=['POST'])
 def submit_flow():
@@ -16,19 +18,23 @@ def submit_flow():
         return 'success'
     return 'failed'
 
-@app.route('/run_flow', method=['POST'])
+
+@app.route('/run_flow', methods=['POST'])
 def run_flow():
     if request.method == 'POST':
         flow_id = request.json['flow_id']
         SchedulerFactory.run_flow(flow_id)
 
-@app.route('/log_flow', method=['POST'])
+
+@app.route('/log_flow', methods=['POST'])
 def log_flow():
     if request.method == 'POST':
         flow_id = request.json['flow_id']
 
+
 if __name__ == '__main__':
     app.debug = True
+    log_center2 = LogCenter()
     app.run(host='127.0.0.1', port=5000, threaded=True)
     SchedulerFactory.shutdown()
     
